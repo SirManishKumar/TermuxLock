c0cb407db5fef93fd60eb85e52d5f4bda59ae4e7
Manish Kumar <79128476+SirManishKumar@users.noreply.github.com>
2021-04-10 10:40:20 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,12 @@ pkg update -y
 pkg upgrade -y
 pkg install figlet
 clear
+echo -e "\e[32m"
+cat $HOME/txt.txt
+echo ""
+echo "This is a tool to add lock to termux"
+echo -e "\e[11m"
+clear
 echo "Enter Your name"
 read username
 clear
